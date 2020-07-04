f53903645d835c92ec7f56d1699326fb90663082
jyj94 <yicho9412@gmail.com>
2020-07-05 05:46:50 +0900

    2020.07.04 - 2
    search 상세 구현(title, content, tag를 통해 검색 가능/대소문자 가능), post_view 기본 구조 구현

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Category
+from django.db.models import Q
 from django.core.paginator import Paginator
 # Create your views here.
 def index(request):
@@ -21,15 +22,24 @@ def search(request):
     keyword = request.GET.get('keyword')
     post_result = []
     if keyword:
-        post_result=list(post_list.filter(title=keyword))
+        #post_result=list(post_list.filter(Q(tag__icontains=keyword)))
+        post_result=list(post_list.filter(Q(title__icontains=keyword) | Q(content__icontains=keyword) | Q(tag__icontains=keyword)))
         print(post_result)
     return render(request, 'search.html', {'post_result' : post_result, 'keyword': keyword})
 
 def category(request):
-    return render(request, 'category.html')
+    post_list = Post.objects.all().order_by('-id')
+    category = request.GET.get('category')
+    category = Category.objects.get(category_name=category)
+    post_result=list(post_list.filter(category=category))
+    return render(request, 'category.html', {'post_result' : post_result})
 
 def post_view(request):
-    return render(request, 'post_view.html')
+    post_info = request.GET.get('post_info')
+    post = Post.objects.filter(id=post_info)[0]
+    print(post)
+    print(post_info)
+    return render(request, 'post_view.html', {'post':post})
 
 def post_write(request):
     return render(request, 'post_write.html')
